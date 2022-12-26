719f447f15193c3f4783f0bc2ea3c36329af02d0
Andras Komjathy <38752953+andraskomjathy@users.noreply.github.com>
2022-12-26 22:58:57 +0100

    Add files via upload

---
+++ Select Columns.py
@@ -0,0 +1,18 @@
+##SELECT * FROM dataset
+
+dataset
+
+##SELECT col1 FROM dataset
+
+dataset.col1
+
+##SELECT col1, col2 FROM dataset
+
+dataset[['col1','col2']]
+
+##SELECT 1,2,3 FROM dataset - Select the first 3 columns
+
+dataset[dataset.columns[0:3]]
+
+
+
